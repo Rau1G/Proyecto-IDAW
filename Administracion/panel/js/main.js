e949440a6ae8c7edea49aea221dcb86faa428cc0
Carolina <carohdezcas@gmail.com>
2021-12-09 20:04:44 -0600

    SE agregó el área de administración

---
+++ Administracion/panel/js/main.js
@@ -0,0 +1,97 @@
+const html = document.documentElement;
+const body = document.body;
+const menuLinks = document.querySelectorAll(".admin-menu a");
+const collapseBtn = document.querySelector(".admin-menu .collapse-btn");
+const toggleMobileMenu = document.querySelector(".toggle-mob-menu");
+const switchInput = document.querySelector(".switch input");
+const switchLabel = document.querySelector(".switch label");
+const switchLabelText = switchLabel.querySelector("span:last-child");
+const collapsedClass = "collapsed";
+const lightModeClass = "light-mode";
+
+/*TOGGLE HEADER STATE*/
+collapseBtn.addEventListener("click", function () {
+  body.classList.toggle(collapsedClass);
+  this.getAttribute("aria-expanded") == "true"
+    ? this.setAttribute("aria-expanded", "false")
+    : this.setAttribute("aria-expanded", "true");
+  this.getAttribute("aria-label") == "collapse menu"
+    ? this.setAttribute("aria-label", "expand menu")
+    : this.setAttribute("aria-label", "collapse menu");
+});
+
+/*TOGGLE MOBILE MENU*/
+toggleMobileMenu.addEventListener("click", function () {
+  body.classList.toggle("mob-menu-opened");
+  this.getAttribute("aria-expanded") == "true"
+    ? this.setAttribute("aria-expanded", "false")
+    : this.setAttribute("aria-expanded", "true");
+  this.getAttribute("aria-label") == "open menu"
+    ? this.setAttribute("aria-label", "close menu")
+    : this.setAttribute("aria-label", "open menu");
+});
+
+/*SHOW TOOLTIP ON MENU LINK HOVER*/
+for (const link of menuLinks) {
+  link.addEventListener("mouseenter", function () {
+    if (
+      body.classList.contains(collapsedClass) &&
+      window.matchMedia("(min-width: 768px)").matches
+    ) {
+      const tooltip = this.querySelector("span").textContent;
+      this.setAttribute("title", tooltip);
+    } else {
+      this.removeAttribute("title");
+    }
+  });
+}
+
+/*TOGGLE LIGHT/DARK MODE*/
+if (localStorage.getItem("dark-mode") === "false") {
+  html.classList.add(lightModeClass);
+  switchInput.checked = false;
+  switchLabelText.textContent = "Light";
+}
+
+switchInput.addEventListener("input", function () {
+  html.classList.toggle(lightModeClass);
+  if (html.classList.contains(lightModeClass)) {
+    switchLabelText.textContent = "Light";
+    localStorage.setItem("dark-mode", "false");
+  } else {
+    switchLabelText.textContent = "Dark";
+    localStorage.setItem("dark-mode", "true");
+  }
+});
+
+/*  Grafica */
+//se saca la referencia al elemento  canvas del DOM
+const $grafica = document.querySelector("#graficaGanacias");
+const etiquetas = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+// Podemos tener varios conjuntos de datos. Comencemos con uno
+const datosVentas2020 = {
+    label: "Ventas por mes",
+    data: [5000, 1500, 8000, 5102, 1200, 4500, 10000, 12000, 19000, 1000, 1500, 12000], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+    backgroundColor: 'rgba(54, 162, 235, 0.2)', // Color de fondo
+    borderColor: 'rgba(54, 162, 235, 1)', // Color del borde
+    borderWidth: 1,// Ancho del borde
+};
+new Chart($grafica, {
+    type: 'line',// Tipo de gráfica
+    data: {
+        labels: etiquetas,
+        datasets: [
+            datosVentas2020,
+            // Aquí más datos...
+        ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+        },
+    }
+});
---
+++ Administracion/panel/templates/menu.php
@@ -0,0 +1,72 @@
+
+<body>
+  <header class="page-header">
+    <nav>
+      <a href="#" class="logo">
+        <img src="img/uaz-logo.png" alt="">
+        <h3>Universidad Autonoma de Zacatecas</h3>
+      </a>
+      <button class="toggle-mob-menu" aria-expanded="false" aria-label="open menu">
+        <svg width="20" height="20" aria-hidden="true">
+          <use xlink:href="#down"></use>
+        </svg>
+      </button>
+      <ul class="admin-menu">
+        <li class="menu-heading">
+          <h3>Administración</h3>
+        </li>
+        <li>
+          <a href="#0">
+            <i class="fas fa-tachometer-alt"></i>
+            <span>Panel de control</span>
+          </a>
+        </li>
+        <li>
+          <a href="#0">
+            <i class="fas fa-user"></i>
+            <span>Usuarios</span>
+          </a>
+        </li>
+        <li>
+          <a href="#0">
+            <i class="fas fa-search-dollar"></i>
+            <span>Ventas</span>
+          </a>
+        </li>
+        <li>
+          <a href="#0">
+            <i class="fas fa-utensils"></i>
+            <span>Productos</span>
+          </a>
+        </li>
+        <li class="menu-heading">
+          <h3>Configuración</h3>
+        </li>
+        <li>
+          <a href="#0">
+            <i class="fas fa-cog"></i>
+            <span>Opticiones</span>
+          </a>
+        </li>
+        <li>
+          <a href="#0">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Salir</span>
+          </a>
+        </li>
+        <li>
+          <div class="switch">
+            <input type="checkbox" id="mode" checked>
+            <label for="mode">
+              <span></span>
+              <span>Dark</span>
+            </label>
+          </div>
+          <button class="collapse-btn" aria-expanded="true" aria-label="collapse menu">
+            <i class="fas fa-arrows-alt-h"></i>
+            <span>Colapsar</span>
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
