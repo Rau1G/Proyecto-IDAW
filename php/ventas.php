e5d2666d58377791dccc8de61fffdcb99cda6f96
MarcoDeAvila <90593381+MarcoDeAvila@users.noreply.github.com>
2021-12-09 18:35:45 -0600

    Archivo PHP de ventas estable

--- php/ventas.php
+++ php/ventas.php
@@ -14,15 +14,6 @@ if (isset($_POST['Paletas'])){ //Paletas
             'precio' => $precio,
             'cantidad' => $cantidad
         ];
-<<<<<<< HEAD
-        $paletas[5] = $sabores;
-    }if($_POST['Paleta_Fresa'] != "-1"){
-        $datos = $_POST['Paleta_Fresa'];
-        $precio = intval(ltrim(explode(" ", $datos)[count(explode(" ", $datos))-1], '$'));
-        $nombre = ltrim(explode(" ", $datos)[count(explode(" ", $datos))-2], ':');
-        $cantidad = intval(ltrim(explode(" ", $datos)[count(explode(" ", $datos)) -5], '$'));
-        
-=======
         $paletas[] = $sabores;
     }
 
@@ -31,24 +22,12 @@ if (isset($_POST['Paletas'])){ //Paletas
         $precio = intval(ltrim(explode(" ", $datos)[count(explode(" ", $datos))-1], '$'));
         $nombre = "Paleta/Fresa";
         $cantidad = intval(explode(" ", $datos)[0]);
-<<<<<<< HEAD
 
-=======
-        echo "<pre>";
-        echo $precio;
-        echo $cantidad;
-        echo "<pre>";
-        echo "<br>".$datos."> Aqui";
->>>>>>> 464a02be961cdc51062c1dd173721c9691b88b10
->>>>>>> 72837c00e3ba8d1b7bcc44702351756591f71688
         $sabores = [
             'nombre' => $nombre,
             'precio' => $precio,
             'cantidad' => $cantidad
         ];
-<<<<<<< HEAD
-        $paletas[0] = $sabores;
-=======
         $paletas[] = $sabores;
     }
 
@@ -60,7 +39,6 @@ if (isset($_POST['Paletas'])){ //Paletas
             die(mysqli_error($conectionSQL));
             echo "Error";
         }
->>>>>>> 464a02be961cdc51062c1dd173721c9691b88b10
     }
 
     header("Location:../Paletas.html");
@@ -85,4 +63,3 @@ if (isset($_POST[''])){ //Bebidas
     
     
 }
-
